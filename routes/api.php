408208bd9666ef6dec2378a69f5ee188776269cd
Vhon Angelico C. Salilo <vhonsalilo1@gmail.com>
2026-02-18 22:15:12 +0800

    Authorization

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('students')->group(function () {
-    Route::get('/', [StudentController::class, 'index']);           // Read all
-    Route::post('/', [StudentController::class, 'store']);          // Create
-    Route::get('/{student}', [StudentController::class, 'show']);      // Read single
-    Route::put('/{student}', [StudentController::class, 'update']);    // Update
-    Route::delete('/{student}', [StudentController::class, 'destroy']); // Delete
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::prefix('students')->group(function () {
+        Route::get('/', [StudentController::class, 'index']);           // Read all
+        Route::post('/', [StudentController::class, 'store']);          // Create
+        Route::get('/{student}', [StudentController::class, 'show']);      // Read single
+        Route::put('/{student}', [StudentController::class, 'update']);    // Update
+        Route::delete('/{student}', [StudentController::class, 'destroy']); // Delete
+    });
 });
